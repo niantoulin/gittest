97c9fc9c7204e231f8dcaf46f0baf0fdbd8a4581
lin <623516574@qq.com>
2022-01-10 22:59:20 +0800

    frist

---
+++ src/main/java/com/example/gittest/demo/test/Test.java
@@ -0,0 +1,12 @@
+package com.example.gittest.demo.test;
+
+/**
+ * @author lin
+ * @create 2022-01-10 22:56
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println(11111111);
+    }
+}
